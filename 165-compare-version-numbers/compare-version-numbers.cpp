f60f56c0dc6302e038001098a3d53ca504adb7cd
Arsh Jaiswal <arshjaiswal7668@gmail.com>
2025-09-23 21:28:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (48.40%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int i = 0, j = 0;
+        int n1 = version1.size(), n2 = version2.size();
+        while (i < n1 || j < n2) {
+            long num1 = 0, num2 = 0;
+            while (version1[i] != '.' && i < n1) {
+                num1 = num1 * 10 + (version1[i] - '0');
+                i++;
+            }
+            while (version2[j] != '.' && j < n2) {
+                num2 = num2 * 10 + (version2[j] - '0');
+                j++;
+            }
+            if (num1 > num2)
+                return 1;
+            if (num1 < num2)
+                return -1;
+            if (i < n1 && version1[i] == '.')
+                i++;
+            if (j < n2 && version2[j] == '.')
+                j++;
+        }
+        return 0;
+    }
+};

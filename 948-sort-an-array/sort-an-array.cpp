e5db10eb341b7cdb2e7cead29e09664c7e3e9fa7
Arsh Jaiswal <arshjaiswal7668@gmail.com>
2026-06-19 22:27:46 +0530

    Time: 686 ms (16.59%) | Memory: 295.8 MB (8.80%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,4 +1,4 @@
-void merge(vector<int> &arr, int low, int mid, int high) {
+void merge(vector<int>& arr, int low, int mid, int high) {
     vector<int> temp;
     int left = low;
     int right = mid + 1;
@@ -6,8 +6,7 @@ void merge(vector<int> &arr, int low, int mid, int high) {
         if (arr[left] <= arr[right]) {
             temp.push_back(arr[left]);
             left++;
-        }
-        else {
+        } else {
             temp.push_back(arr[right]);
             right++;
         }
@@ -23,9 +22,10 @@ void merge(vector<int> &arr, int low, int mid, int high) {
     for (int i = low; i <= high; i++)
         arr[i] = temp[i - low];
 }
-void mergeSort(vector<int> &arr, int low, int high) {
-    if (low >= high) return;
-    int mid = (low + high) / 2 ;
+void mergeSort(vector<int>& arr, int low, int high) {
+    if (low >= high)
+        return;
+    int mid = (low + high) / 2;
     mergeSort(arr, low, mid);
     mergeSort(arr, mid + 1, high);
     merge(arr, low, mid, high);
@@ -33,7 +33,7 @@ void mergeSort(vector<int> &arr, int low, int high) {
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        int n=nums.size();
+        int n = nums.size();
         mergeSort(nums, 0, n - 1);
         return nums;
     }

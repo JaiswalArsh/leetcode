75694483b6692c45e38f456c734feb88ab4f614d
Arsh Jaiswal <arshjaiswal7668@gmail.com>
2026-07-08 23:49:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (49.66%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+    bool isAnagram(string str1, string str2) {
+        if (str1.length() != str2.length())
+            return false;
+        int freq[26] = {0};
+        for (int i = 0; i < str1.length(); i++)
+            freq[str1[i] - 'a']++;
+        for (int i = 0; i < str2.length(); i++)
+            freq[str2[i] - 'a']--;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0)
+                return false;
+        }
+        return true;
     }
 };

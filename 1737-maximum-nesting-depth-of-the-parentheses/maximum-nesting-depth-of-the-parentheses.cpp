5db40b2caa7648b2b1fe308df0165b023256c492
Arsh Jaiswal <arshjaiswal7668@gmail.com>
2026-07-09 00:21:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (7.29%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int p=0;
+        int ans=0;
+        for(auto it:s){
+            if(it=='(') p++;
+            if(it==')') p--;
+            ans=max(ans,p);
+        }
+        return ans;
+    }
+};
